377c70c8155240c408f3817e856068c9cd161f34
hushiyan <hushiyan01@gmail.com>
2023-09-11 09:03:13 -0400

    updated relationship between select service and select time slots

--- app/src/main/java/com/example/barbarbookingapp/viewmodel/AppointmentViewModel.kt
+++ app/src/main/java/com/example/barbarbookingapp/viewmodel/AppointmentViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.barbarbookingapp.viewmodel
 
-class AppointmentViewModel {
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.barbarbookingapp.model.repository.IRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SelectTimeViewModel@Inject constructor() :ViewModel() {
+    private var _selectedStartTime = MutableLiveData<Pair<Int,Int>>()
+    var selectedStartTime = _selectedStartTime
+
+    private var _backGroundColor = MutableLiveData<Color>()
+    var backGroundColor = _backGroundColor
+
+    fun setStartTime(startTime:Pair<Int,Int>){
+        _selectedStartTime.postValue(startTime)
+    }
+
+    fun setBackGroundColor(color: Color){
+        _backGroundColor.postValue(color)
+    }
 }
