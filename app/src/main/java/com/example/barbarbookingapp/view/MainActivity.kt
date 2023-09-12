8f3571a12cf3efc686fc3fd4a8020689600b5e64
yao <yuanyao721@gmail.com>
2023-09-12 00:14:38 -0500

    update route and add appointment list

--- app/src/main/java/com/example/barbarbookingapp/view/MainActivity.kt
+++ app/src/main/java/com/example/barbarbookingapp/view/MainActivity.kt
@@ -7,11 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.barbarbookingapp.view.appointment_screens.UserAppointmentsScreen
 import com.example.barbarbookingapp.view.intro_screens.Login
 import com.example.barbarbookingapp.view.intro_screens.SignUp
 import com.example.barbarbookingapp.view.intro_screens.SplashScreen
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
                     MyApp()
                 }
--- app/src/main/java/com/example/barbarbookingapp/view/appointment_screens/AppointmentDetails.kt
+++ app/src/main/java/com/example/barbarbookingapp/view/appointment_screens/AppointmentDetails.kt
@@ -1,7 +1,11 @@
 package com.example.barbarbookingapp.view.appointment_screens
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +16,15 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,134 +37,198 @@ import com.example.barbarbookingapp.viewmodel.BarberViewModel
 
 @Composable
 fun AppointmentDetails(viewModel: BarberViewModel, appointmentId: Int, navController: NavController){
-    val services = listOf(
-        Service(1, "Haircut", 30, 40.0),
-        Service(2, "Massage", 60, 100.0)
-    )
+    val context = LocalContext.current
+    LaunchedEffect(key1 = true) {
+        viewModel.selectedAppointmentId(appointmentId)
+    }
+    val appointmentWithServices by viewModel.selectedAppointmentWithServices.observeAsState(null)
+    val barber by viewModel.selectedBarber.observeAsState(null)
+    appointmentWithServices?.let { appointmentDetails ->
+        LaunchedEffect(key1 = appointmentDetails.appointment.barberId) {
+            viewModel.selectedBarberId(appointmentDetails.appointment.barberId)
+        }
+    }
+    val services = appointmentWithServices?.services
+
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)) {
-        val (dateTimeTitle, dateText, timeText, idText, statusText, barberTitle, barberName, serviceList, billText ) = createRefs()
-        Text(
-            modifier = Modifier
-                .constrainAs(idText){
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                },
-            fontSize = 25.sp,
-            text = stringResource(R.string.appointment_id)
-        )
+            .padding(20.dp)) {
+        val (dateTimeTitle, dateText, timeText, idText, statusText, barberTitle, barberCard, serviceTitle, serviceList, billText ) = createRefs()
+        appointmentWithServices?.let {
+            Text(
+                modifier = Modifier
+                    .constrainAs(idText){
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    },
+                fontSize = 25.sp,
+                text = "Appointment #${appointmentWithServices!!.appointment.appointmentId}"
+            )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(statusText) {
-                    top.linkTo(idText.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 10.dp),
-            fontSize = 25.sp,
-            color = Color.Red,
-            text = stringResource(R.string.status)
-        )
+            Text(
+                modifier = Modifier
+                    .constrainAs(statusText) {
+                        top.linkTo(idText.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 10.dp),
+                fontSize = 25.sp,
+                color = Color.Red,
+                text = appointmentWithServices!!.appointment.status.toString()
+            )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(dateTimeTitle) {
-                    top.linkTo(statusText.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 10.dp),
-            fontSize = 25.sp,
-            text = stringResource(R.string.date_time)
-        )
+            Text(
+                modifier = Modifier
+                    .constrainAs(dateTimeTitle) {
+                        top.linkTo(statusText.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 10.dp),
+                fontSize = 25.sp,
+                color = Color.Gray,
+                text = stringResource(R.string.date_time)
+            )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(dateText) {
-                    top.linkTo(dateTimeTitle.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 5.dp),
-            fontSize = 20.sp,
-            text = stringResource(R.string.date)
-        )
+            Text(
+                modifier = Modifier
+                    .constrainAs(dateText) {
+                        top.linkTo(dateTimeTitle.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 5.dp),
+                fontSize = 20.sp,
+                text = appointmentWithServices!!.appointment.appointmentDate
+            )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(timeText) {
-                    top.linkTo(dateText.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 5.dp),
-            fontSize = 20.sp,
-            text = stringResource(R.string.time)
-        )
+            Text(
+                modifier = Modifier
+                    .constrainAs(timeText) {
+                        top.linkTo(dateTimeTitle.bottom)
+                        start.linkTo(dateText.end, 10.dp)
+                    }
+                    .padding(0.dp, 5.dp),
+                fontSize = 20.sp,
+                text = appointmentWithServices!!.appointment.appointmentTime
+            )
 
-        Text(
-            modifier = Modifier
-                .constrainAs(barberTitle) {
-                    top.linkTo(timeText.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 10.dp),
-            fontSize = 25.sp,
-            text = stringResource(R.string.barber)
-        )
-
-        Text(
-            modifier = Modifier
-                .constrainAs(barberName) {
-                    top.linkTo(barberTitle.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 5.dp),
-            fontSize = 20.sp,
-            text = stringResource(R.string.barber_name)
-        )
-
-        Column(
-            modifier = Modifier
-                .constrainAs(serviceList) {
-                    top.linkTo(barberName.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 10.dp)
-        ) {
-            services.forEach {
+            Text(
+                modifier = Modifier
+                    .constrainAs(barberTitle) {
+                        top.linkTo(timeText.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 10.dp),
+                fontSize = 25.sp,
+                color = Color.Gray,
+                text = stringResource(R.string.barber)
+            )
+            barber?.let {
                 Card(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    shape = RoundedCornerShape(5.dp),
+                        .fillMaxWidth()
+                        .constrainAs(barberCard) {
+                            top.linkTo(barberTitle.bottom)
+                            start.linkTo(parent.start)
+                        }
+                        .padding(0.dp, 5.dp),
+                    shape = RoundedCornerShape(10.dp),
                     border = BorderStroke(1.dp, Color.Gray),
                     colors = CardDefaults.cardColors(containerColor = Color.Transparent)
                 ) {
-                    Text(
-                        modifier = Modifier.padding(5.dp),
-                        text = it.name
-                    )
-                    Text(
-                        modifier = Modifier.padding(5.dp),
-                        text = "${it.duration.toString()} Minutes"
-                    )
-                    Text(
-                        modifier = Modifier.padding(5.dp),
-                        text = "$ ${it.price.toString()}"
-                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp, 10.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            fontSize = 20.sp,
+                            text = "${barber!!.firstName} ${barber!!.lastName}"
+                        )
+                        val resourceId = context.resources.getIdentifier(barber!!.image, "drawable", context.packageName)
+                        Image(
+                            modifier = Modifier.size(80.dp),
+                            painter = painterResource(id = resourceId),
+                            contentDescription = "barber"
+                        )
+                    }
                 }
-                Spacer(modifier = Modifier.size(5.dp))
             }
+
+            Text(
+                modifier = Modifier
+                    .constrainAs(serviceTitle) {
+                        top.linkTo(barberCard.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 10.dp),
+                fontSize = 25.sp,
+                color = Color.Gray,
+                text = stringResource(R.string.services)
+            )
+
+            Column(
+                modifier = Modifier
+                    .constrainAs(serviceList) {
+                        top.linkTo(serviceTitle.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 10.dp)
+            ) {
+                services?.forEach {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(8.dp),
+                        border = BorderStroke(1.dp, Color.Gray),
+                        colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(15.dp, 10.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            val resourceId = context.resources.getIdentifier(it.image, "drawable", context.packageName)
+                            Image(
+                                modifier = Modifier.size(80.dp),
+                                painter = painterResource(id = resourceId),
+                                contentDescription = "barber"
+                            )
+                            Spacer(modifier = Modifier.size(30.dp))
+                            Column {
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = it.name
+                                )
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = "${it.duration.toString()} Minutes"
+                                )
+                                Text(
+                                    modifier = Modifier.padding(5.dp),
+                                    text = "$ ${it.price.toString()}"
+                                )
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(5.dp))
+                }
+            }
+            Text(
+                modifier = Modifier
+                    .constrainAs(billText) {
+                        top.linkTo(serviceList.bottom)
+                        start.linkTo(parent.start)
+                    }
+                    .padding(0.dp, 10.dp),
+                fontSize = 25.sp,
+                text = "Total Cost: $ ${appointmentWithServices!!.appointment.serviceCharge.toString()}"
+            )
         }
 
-        Text(
-            modifier = Modifier
-                .constrainAs(billText) {
-                    top.linkTo(serviceList.bottom)
-                    start.linkTo(parent.start)
-                }
-                .padding(0.dp, 10.dp),
-            fontSize = 25.sp,
-            text = stringResource(R.string.total_Cost)
-        )
+
     }
 }
